ac3f53ace63fd96f2378bb0c3ac4f02bd86eb5f9
Mhac Alester Montera <mhacalester.montera@gmail.com>
2026-06-28 03:49:05 +0800

    [chore] migrate app entry to expo-router

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
+
+  if (!loaded) return null;
+
+  return <Stack />;
+}
